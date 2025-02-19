cd977b0e52c187bdceac8e28b8876a0c2c54f975
Jobaer Islam <jobaerislam16@gmail.com>
2025-02-19 20:32:49 +0600

    4th solve

---
+++ problem-4.js
@@ -0,0 +1,16 @@
+function  validProposal( person1 , person2 ) {
+    if(typeof person1 !== 'object' || person2 !== 'object') {
+        return "Invalid";
+    }
+    if(person1.gender !== person2.gender && Math.abs(person1.age - person2.age) <= 7) {
+        return true;
+    }
+    return false;
+
+
+}
+const ami = validProposal(
+    { name: "toya", gender: "female", age: 20 },
+    { name: "kader", gender: "male", age: 25 }
+  );
+console.log(ami);
